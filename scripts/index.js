e37a6dac22f52126d19803eb6053d98586003466
Almar_Block <98546933+AlmarBlock@users.noreply.github.com>
2023-12-05 17:17:55 +0100

    added fast pagefinder to index.html

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,22 @@ for (let i = 1; i <= 24; i++) {
         AufgabeDesTages.style.display = "block";
     }
 }
+
+input = document.getElementById('textbox_id')
+input.max = today
+
+function loadpage() {
+    value = document.getElementById('textbox_id').value
+    if (value < 1 || value > today || value == null) {
+        alert("Bitte geben sie eine Zahl zwischen 1 und " + today + " ein.")
+    }
+    else {
+        window.location.href = './pages/day' + value + '.html'
+    }
+}
+
+input.addEventListener("keydown", function (e) {
+    if (e.code === "Enter") {
+        loadpage();
+    }
+});
