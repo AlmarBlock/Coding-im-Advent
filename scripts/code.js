a2f7988fdebce7739dd2c8a1a9c4aaf7562e8f4f
Almar_Block <98546933+AlmarBlock@users.noreply.github.com>
2023-12-14 09:40:28 +0100

    added int do code

--- scripts/code.js
+++ scripts/code.js
@@ -10,7 +10,7 @@ let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9','len'];
 let operators = ['not', 'or', 'and', 'var', 'const', 'let', 'func', 'def'];
 let specialOperators = ['True', 'False', 'None'];
 let varibles = ['i', 'j', 'Blaetter', 'Blatt_war_auf_dem_weg', 'hoehe', 'breite', 'Variable', 'array', 'Variable1', 'Variable2', 'mushrooms', 'trees', 'anweisungen', 'number', 'zahl', 'turn', 'input', 'grade'];
-let specialKeywords = ['range', 'str'];
+let specialKeywords = ['range', 'str', 'int'];
 let strings = ['"True"', '"1"'];
 let functs = ['append', 'print', '_goToMushroom', '_goToPilz', '_mushroomLeft', '_TreeLeft'];
 
