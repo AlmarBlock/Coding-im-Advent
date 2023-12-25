7b2cfbbb4349cc13b5dc8abddeee95689d1c4ce0
Almar_Block <98546933+AlmarBlock@users.noreply.github.com>
2023-12-25 16:15:24 +0100

    Clearafis toggle.js

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -4,18 +4,21 @@ let yearText = document.getElementsByClassName("2023");
 const directJump = document.getElementsByTagName('direktjump')[0]
 
 if (month == 12) {
+    //NOTE - Changes all thigs after the 24th of December
     if (today > 24) {
         directJump.style.display = "none";
     }
     else {
         directJump.style.display = "block";
     }
+    //NOTE - Changes all thigs while it is December
     timer.style.display = "none";
     years.style.display = "none";
     for(let i = 0; i < yearText.length; i++) {
       yearText[i].style.display = "block";
     }
 } else {
+    //NOTE - Changes all thigs before/after December
     for(let i = 0; i < yearText.length; i++) {
       yearText[i].style.display = "none";
     }
